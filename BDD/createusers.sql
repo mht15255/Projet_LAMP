aad7eb63fbfe943bc9a4eacb8e533f5a10661a70
lanuitdestps <florentgaudel@gmail.com>
2017-09-26 17:40:24 +0200

    Update createusers.sql

--- BDD/createusers.sql
+++ BDD/createusers.sql
@@ -1,6 +1,8 @@
 
 CREATE USER 'prestataire'@'%' IDENTIFIED BY 'prestataire';
 CREATE USER 'concess'@'localhost' IDENTIFIED BY 'concess';
+CREATE USER 'prestataire'@'localhost' IDENTIFIED BY 'prestataire';
+CREATE USER 'concess'@'%' IDENTIFIED BY 'concess';
 GRANT select ON libautodb.* TO 'concess'@'localhost';
 GRANT select ON libautodb.* TO 'prestataire'@'localhost';
 GRANT select ON libautodb.* TO 'concess'@'%';
