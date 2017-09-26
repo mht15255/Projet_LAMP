4866a0732a6aca809fefd0cc22c9e78bbed89887
ghizlane85 <32061509+ghizlane85@users.noreply.github.com>
2017-09-26 19:47:42 +0200

    Update createusers.sql

--- BDD/createusers.sql
+++ BDD/createusers.sql
@@ -1,9 +1,9 @@
 
 CREATE USER 'prestataire'@'%' IDENTIFIED BY 'prestataire';
-CREATE USER 'concess'@'localhost' IDENTIFIED BY 'concess';
+CREATE USER 'concessionnaire'@'localhost' IDENTIFIED BY 'concess';
 CREATE USER 'prestataire'@'localhost' IDENTIFIED BY 'prestataire';
-CREATE USER 'concess'@'%' IDENTIFIED BY 'concess';
-GRANT select ON libautodb.* TO 'concess'@'localhost';
+CREATE USER 'concessionnaire'@'%' IDENTIFIED BY 'concess';
+GRANT select ON libautodb.* TO 'concessionnaire'@'localhost';
 GRANT select ON libautodb.* TO 'prestataire'@'localhost';
-GRANT select ON libautodb.* TO 'concess'@'%';
+GRANT select ON libautodb.* TO 'concessionnaire'@'%';
 GRANT select ON libautodb.* TO 'prestataire'@'%';
